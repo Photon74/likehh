506e59aa87eefaa19e55ff9a72f749636f02eaad
Alexandr Kolodinsky <Photon.74@gmail.com>
2023-03-13 20:01:58 +0300

    maui

---
+++ MauiFront/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiFront;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
